cfaafbdfe81f380231ede380e0e9cef96d5914e7
alex4o <mrabonin@gmail.com>
2015-03-17 10:19:16 +0200

    navigation should show where the user is

--- javascript/t.js
+++
@@ -1,8 +0,0 @@
-var InputView = React.createClass({
-	componentDidMount: function() {
-		katex.render(this.props.math,this.refs.equ.getDOMNode())
-	},
-	render: function(){
-		return(React.createElement("div",{},React.createElement(NumberInput,null)))
-	}
-})
