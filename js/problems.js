fe0283cb4cdd65e1092b54ec9fd503c9f4891e42
alex4o <mrabonin@gmail.com>
2015-04-02 15:51:56 +0300

    forgot to add problems result

---
+++ js/problems.js
@@ -0,0 +1,41 @@
+var Problems = React.createClass({displayName: "Problems",
+	getInitialState:function() {
+		return {
+		  list: [],
+		  starlist: []
+		};
+	},
+	componentDidMount: function () {
+		self = this
+	    superagent.get("/api/data/problems/").end(function(){
+
+	    })  
+	},
+	render:function() {
+		return (
+		
+		React.createElement("div", null, 
+			React.createElement("h1", null, "Генерирани задачи"), 
+			React.createElement("div", {className: "gen-list"}, 
+				React.createElement(ProblemListItem, null), 
+				React.createElement(ProblemListItem, null), 
+				React.createElement(ProblemListItem, null)
+
+			)
+		)
+		);
+	}
+});
+
+var ProblemListItem = React.createClass({displayName: "ProblemListItem",
+    render: function () {
+        return (
+            React.createElement("div", {className: "gen-item"}, 
+				React.createElement(KaТeXitem, {problem: "ax^2-bx+c"}), 
+				React.createElement("span", {className: "control"}, "Любима"), 
+				React.createElement("span", {className: "control"}, "Изтрий")
+			)
+        );
+    }
+});
+
